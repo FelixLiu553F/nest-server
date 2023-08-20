4480c66d23f85205dede37ccafcae9ec10af31ac
刘敬峰 <felixliu553@gmail.com>
2023-08-20 19:32:43 +0800

    feat: i18n init

---
+++ src/common/i18n/index.ts
@@ -0,0 +1,60 @@
+// export const langs: { [key: string]: I18NSettings } = {
+//   'zh-CN': cn,
+//   'en-US': en,
+// };
+
+// export interface I18NSettings {
+//   language: AppLanguage;
+//   user: {
+//     notExist: string;
+//     wrongPwdOrUsername: string;
+//     passwordRequired: string;
+//     emailExists: string;
+//     usernameExists: string;
+//     socialBinded: string;
+//     passwordError: string;
+//   };
+//   inviteUserJoinImean: {
+//     subject: string;
+//     title: string;
+//     content(name: string): string;
+//     inviteTitle: string;
+//     imeanIs: string;
+//     aboutDocument: string;
+//     button: string;
+//   };
+//   emailVerify: {
+//     subject: string;
+//     wrongCode: string;
+//     expiredCode: string;
+//     emailContent(code: string): string;
+//   };
+//   folder: {
+//     defaultName: string;
+//     loginRequired: string;
+//     cannotMoveToSameFolder: string;
+//     cannotMoveToDisableFolder: string;
+//     cannotMoveToSubFolder: string;
+//   };
+//   recording: {
+//     notExist: string;
+//     untitled: string;
+//     cannotMoveToDisableFolder: string;
+//     cannotSaveToDisableFolder: string;
+//   };
+//   invitation: {
+//     alreadyJoined: string;
+//     mail: {
+//       subject: string;
+//       title: string;
+//       greeting(user: string): string;
+//       content(user: string, targetName: string, type: MemberType): string;
+//       button: string;
+//       tips: string;
+//     };
+//   };
+//   errors: { [key in BusinessErrorCodeKeys]: string };
+//   upload: {
+//     fileSizeExceeded(size: number): string;
+//   };
+// }
